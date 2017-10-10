f36d78177394e0ab9423002aad4398d529f511c1
Docinho <livia.juliao@ccc.ufcg.edu.br>
2017-10-09 19:05:22 -0500

    alguns plots

--- guardians1.R
+++ guardians1.R
@@ -11,9 +11,32 @@ dados <- read_csv("logs.txt", col_names = c("mes", "dia_do_mes", "hora", "maquin
 dados <- dados %>% mutate(data = paste("2017", mes, dia_do_mes, sep = "-"), dia_da_semana = wday(data, label = T))
 sessoes_abertas <- dados %>% filter(status == "opened")
 
+# acessos pelo dia do mes e da semana
 sessoes <- sessoes_abertas %>% subset(select=c("dia_da_semana", "dia_do_mes"))
-sessoes <- sessoes %>% data.frame()
+freq_dia <- sessoes %>% group_by(dia_da_semana, dia_do_mes) %>% summarise(num_acessos = n())
+freq_dia %>% ggplot(aes(x = dia_do_mes, y = num_acessos, fill = dia_da_semana)) + geom_bar(stat = "identity")
 
-#plotar o grafico
-sessoes_abertas %>% ggplot(aes(x = dia_da_semana)) +  geom_bar()
-sessoes %>% ggplot(aes(x = dia_da_semana)) +  geom_bar()
+# acessos no mês de acordo com o dia da semana
+freq_mensal <- freq_dia %>% group_by(dia_da_semana) %>% summarise(media_acessos = mean(num_acessos))
+freq_mensal %>% ggplot(aes(x = dia_da_semana, y = media_acessos)) + geom_bar(stat = "identity")
+
+# extraindo colunas do lab, da "hora pura", do turno
+r <- setNames(do.call(rbind.data.frame, strsplit(sessoes_abertas$maquina, "-")), c("lab", "maquina"))
+sessoes_abertas <- sessoes_abertas %>% mutate(lab = r$lab)
+sessoes_abertas <- sessoes_abertas %>% mutate(hora_pura = hour(sessoes_abertas$hora))
+sessoes_abertas <- sessoes_abertas %>% mutate(turno = if_else(hora_pura %in% 05:11, "manha",
+                                                              if_else(hora_pura %in% 12:17, "tarde",
+                                                                      if_else(hora_pura %in% 18:24, "noite", "madrugada"))))
+
+# graficos do turno de acesso por lcc
+sessoes_abertas %>% ggplot(aes(x = lab)) + geom_bar()
+sessoes_abertas %>% ggplot(aes(x = turno)) + geom_bar()
+sessoes_abertas %>% ggplot(aes(x = hora_pura)) + geom_bar() # ao contrario do que se esperava, 
+
+#o horario de mais acesso ao lcc é as 10 hrs
+lcc2_acesso <- sessoes_abertas %>% filter(lab == "lcc2")
+lcc2_acesso %>% ggplot(aes(x = hora_pura)) + geom_bar()
+
+lcc1_acesso <- sessoes_abertas %>% filter(lab == "lcc1")
+lcc1_acesso %>% ggplot(aes(x = hora_pura)) + geom_bar() # maior quantudade de acesso as 14 hrs
+lcc1_acesso %>% ggplot(aes(x = dia_da_semana)) + geom_bar()
